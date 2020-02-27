67baaadea55b73d0bade40fcc71d244e1905a44c
Vaishali Dhanoa <vaishali.dhanoa@gmail.com>
2020-02-27 09:36:27 +0100

    Fix Output Impact

--- src/taskpane/operations/impact.ts
+++ src/taskpane/operations/impact.ts
@@ -6,11 +6,10 @@
 // work on output
 // remove duplicates
 
-import { std, abs } from 'mathjs';
+import { abs } from 'mathjs';
 import CellProperties from '../cellproperties';
 import CommonOperations from './commonops';
 import SheetProperties from '../sheetproperties';
-import { increment } from 'src/functions/functions';
 
 export default class Impact {
 
@@ -96,15 +95,13 @@ export default class Impact {
   private addImpactInfo(n: number = 1) {
 
     this.addImpactInfoInputCells(n);
-    this.addImpactInfoOutputCells();
+    this.addImpactInfoOutputCells(this.referenceCell, n);
   }
 
   private addImpactInfoInputCells(n: number = 1) {
 
-    const isreferenceCellAverage = this.referenceCell.formula.includes("AVERAGE") || this.referenceCell.formula.includes("MITTELWERT");
-    const isreferenceCellSum = this.referenceCell.formula.includes("SUM");
     const isreferenceCellDiff = this.referenceCell.formula.includes('-');
-    const divisor = this.getDivisor();
+    const divisor = this.getDivisor(this.referenceCell);
     let subtrahend = null;
 
 
@@ -144,111 +141,62 @@ export default class Impact {
     })
   }
 
-  private addImpactInfoOutputCells() {
+  private addImpactInfoOutputCells(cell: CellProperties, n: number) {
 
-    this.referenceCell.outputCells.forEach((outCell: CellProperties) => {
-
-      let isSubtrahend: boolean = false;
-      let isMinuend: boolean = false;
+    cell.outputCells.forEach((outCell: CellProperties) => {
 
       if (outCell.formula.includes('-')) {
-        //figure out whether the reference cell is minuend or subtrahend to the outcell
+        const divisor = this.getDivisor(outCell);
         let formula: string = outCell.formula;
         formula = formula.replace('=', '').replace(' ', '');
-        let idx = formula.indexOf('-');
-        let subtrahend = formula.substring(idx + 1, formula.length);
+        const idx = formula.indexOf('-');
+        const subtrahend = formula.substring(idx + 1, formula.length);
 
-        if (this.referenceCell.address.includes(subtrahend)) {
-          isSubtrahend = true;
-        } else {
-          isMinuend = true;
-        }
+        outCell.rectColor = this.checkIfCellHasSubtrahend(cell, subtrahend);
+        outCell.rectTransparency = abs(1 - this.referenceCell.value / divisor);
+
+      } else if (outCell.formula.includes('AVERAGE')) {
+        const divisor = this.getDivisor(outCell);
+        const transparency = abs(1 - this.referenceCell.value / divisor);
+
+        outCell.rectTransparency = transparency;
+        outCell.rectColor = 'green';
+      } else {
+        outCell.rectTransparency = abs(1 - this.referenceCell.value / outCell.value);
+        outCell.rectColor = 'green';
       }
-
-      let colorProperties = this.outputColorProperties(outCell.value, this.referenceCell.value, outCell.inputCells, isSubtrahend, isMinuend);
-      outCell.rectColor = colorProperties.color;
-      outCell.rectTransparency = colorProperties.transparency;
     })
+
+    if (n == 1) {
+      return;
+    }
+
+    n = n - 1;
+    cell.outputCells.forEach((outCell: CellProperties) => {
+      this.addImpactInfoOutputCells(outCell, n);
+    });
+
   }
 
-  private computeColor(cellValue: number, referenceCellValue: number, cells: CellProperties[], isSubtrahend: boolean = false, isMinuend: boolean = false) {
+  private checkIfCellHasSubtrahend(cell: CellProperties, subtrahend: string) {
+    let color = 'green';
 
-    let color = "green";
-
-    if (isSubtrahend) {
-      color = "red";
+    if (cell.address.includes(subtrahend)) {
+      color = 'red';
       return color;
     }
 
-    if (referenceCellValue > 0 && cellValue < 0) {
-      if (isMinuend) {
-        color = "green";
-      } else {
-        color = "red";
-      }
-    }
+    cell.outputCells.forEach((outCell: CellProperties) => {
+      this.checkIfCellHasSubtrahend(outCell, subtrahend);
+    })
 
-    if (referenceCellValue < 0 && cellValue < 0) { // because of the negative sign, the smaller the number the higher it is
-      let isAnyCellPositive = false;
-
-      cells.forEach((cell: CellProperties) => {
-        if (cell.value > 0) {
-          isAnyCellPositive = true;
-        }
-      })
-
-      if (isAnyCellPositive) { // if even one cell is positive, then it means that only that cell is contributing positively and rest all are contributing negatively
-        color = "red";
-      }
-    }
     return color;
   }
 
-  // Fix color values for negative values
-  private outputColorProperties(cellValue: number, referenceCellValue: number, cells: CellProperties[], isSubtrahend: boolean = false, isMinuend: boolean = false) {
-
-    let transparency = 0;
-    const color = this.computeColor(cellValue, referenceCellValue, cells, isSubtrahend, isMinuend);
-
-    // Make both values positive
-    if (cellValue < 0) {
-      cellValue = -cellValue;
-    }
-
-    if (referenceCellValue < 0) {
-      referenceCellValue = -referenceCellValue;
-    }
-
-    if (cellValue > referenceCellValue) {
-
-      let value = referenceCellValue / cellValue;
-
-      transparency = 1 - value;
-
-    }
-    else {
-      let maxValue = cellValue;
-      // go through the input cells of the output cell
-      cells.forEach((c: CellProperties) => {
-        let val = c.value;
-        if (val < 0) {
-          val = -val;
-        }
-        if (val > maxValue) {
-          maxValue = val;
-        }
-      })
-
-      transparency = 1 - (referenceCellValue / maxValue);
-    }
-
-    return { color: color, transparency: transparency };
-  }
-
-  private getDivisor() {
+  private getDivisor(cell: CellProperties) {
     let divisor = 1;
 
-    this.referenceCell.inputCells.forEach((c: CellProperties) => {
+    cell.inputCells.forEach((c: CellProperties) => {
       divisor += c.value;
     })
     return divisor;
