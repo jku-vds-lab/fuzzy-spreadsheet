8af95a573d380efab41f6f37575e5eee526d5350
Vaishali Dhanoa <vaishali.dhanoa@gmail.com>
2020-10-29 15:02:13 +0100

    experiment with different arrows

--- src/taskpane/sheet/whatifproperties.ts
+++ src/taskpane/sheet/whatifproperties.ts
@@ -578,9 +578,9 @@ export default class WhatIfProps extends SheetProp {
 
         let text = '';
 
-        let color = 'red';
+        // let color = 'red';
         if (updatedValue > 0) {
-          color = 'green';
+          // color = 'green';
           text += '+';
         }
 
@@ -589,7 +589,7 @@ export default class WhatIfProps extends SheetProp {
         } else {
           text += updatedValue.toFixed(1);
         }
-        let MARGIN = 26;
+        let MARGIN = 24;
 
         const textbox = sheet.shapes.addTextBox(text);
         textbox.name = cell.address + 'TextBoxUpdate';
@@ -603,24 +603,38 @@ export default class WhatIfProps extends SheetProp {
         textbox.textFrame.verticalAlignment = "Distributed";
         textbox.textFrame.textRange.font.size = 7.5;
 
-        let rotation = 0;
+        let rotation = 0; //45;
 
-        if (color == 'red') {
-          rotation = 180;
+        if (updatedValue < 0) {
+          rotation = 180; // 225;
         }
 
-        let arrowHeight = cell.height / 3;
-        let arrowWidth = 5;
+        let arrowHeight = 8.5;
+        let arrowWidth = 5.9;
 
-        let arrow = sheet.shapes.addGeometricShape(Excel.GeometricShapeType.triangle);
-        arrow.name = cell.address + 'TextBoxUpdate';
-        arrow.width = arrowWidth;
-        arrow.height = arrowHeight;
-        arrow.top = cell.top + (cell.height - arrowHeight) / 2;
-        arrow.left = cell.left + MARGIN;
-        arrow.lineFormat.color = color;
-        arrow.rotation = rotation;
-        arrow.fill.setSolidColor(color);
+        let lineLeft = cell.left + MARGIN + 4;
+        let lineTop = cell.top + (cell.height - arrowHeight) / 2;
+        const line = sheet.shapes.addLine(lineLeft, lineTop, lineLeft, lineTop + arrowHeight, Excel.ConnectorType.straight);
+        line.name = "StraightLine";
+        line.line.beginArrowheadStyle = Excel.ArrowheadStyle.triangle;
+        line.line.beginArrowheadWidth = Excel.ArrowheadWidth.narrow;
+        line.rotation = rotation;
+        line.lineFormat.color = '000000';
+        line.fill.setSolidColor('000000');
+
+        // ARROWS
+        // // let arrow = sheet.shapes.addGeometricShape(Excel.GeometricShapeType.halfFrame);
+        // let arrow = sheet.shapes.addGeometricShape(Excel.GeometricShapeType.upArrow);
+        // arrow.name = cell.address + 'TextBoxUpdate';
+        // arrow.width = arrowWidth;
+        // arrow.height = arrowHeight;
+        // arrow.top = cell.top + (cell.height - arrowHeight) / 2;
+        // arrow.left = cell.left + MARGIN;
+        // // arrow.lineFormat.color = color;
+        // // arrow.fill.setSolidColor(color);
+        // arrow.rotation = rotation;
+        // arrow.lineFormat.visible = false;
+        // arrow.fill.setSolidColor('7F7F7F');
 
         let range = sheet.getRange(this.newReferenceCell.address);
         range.select();
